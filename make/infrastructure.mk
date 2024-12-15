953c2c13db67ed5d8aca19dbed2ba1c118a4a501
Daniel Jones <contact@danieljones.net>
2024-12-15 19:08:18 +1100

    feat: Build templates and cluster nodes

--- make/infrastructure.mk
+++ make/infrastructure.mk
@@ -4,8 +4,8 @@
 
 infra-build: log ## Builds the base level VM infrastructure in Proxmox
 	@echo "🏗️ Building infrastructure"
-	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml -k -v ansible/infrastructure-build.yml
+	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml -v ansible/infrastructure-build.yml
 
 infra-destroy: log ## Destroys the entire VM swarm stack
 	@echo "💣 Destroying infrastructure"
-	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml -k -v ansible/infrastructure-destroy.yml
+	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml -v ansible/infrastructure-destroy.yml
