87efae559f14b2b43ab898621189a9970f26e334
Daniel Jones <contact@danieljones.net>
2024-12-18 22:35:24 +1100

    chore: remove verbose logging from infra make targets

--- make/infrastructure.mk
+++ make/infrastructure.mk
@@ -4,8 +4,8 @@
 
 infra-build: log confirm ## Builds the base level VM infrastructure in Proxmox
 	@echo "🚧 Building infrastructure"
-	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml -vv ansible/infrastructure-build.yml
+	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml ansible/infrastructure-build.yml
 
 infra-destroy: log confirm ## Destroys the entire VM swarm stack, along with all your data
 	@echo "🧨 Destroying infrastructure"
-	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml -v ansible/infrastructure-destroy.yml
+	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml ansible/infrastructure-destroy.yml
