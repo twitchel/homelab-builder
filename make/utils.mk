3b36913c66707f29f37086b3687c885726fe35fc
Daniel Jones <daniel.jones@oohmedia.com.au>
2024-12-27 14:18:09 +1100

    feat: add pre-flight checks for ansible and tofu installation

--- make/utils.mk
+++ make/utils.mk
@@ -2,6 +2,9 @@
 ##@ Utilities
 #####################
 
+ANSIBLE_INSTALLED := $(shell command -v ansible 2> /dev/null)
+TOFU_INSTALLED := $(shell command -v tofu 2> /dev/null)
+
 log: ## log env vars
 	@echo "\n======================================================\n"
 	@echo "ENVIRONMENT          $(ENVIRONMENT)"
@@ -19,6 +22,12 @@ help: log ## This help.
 	@awk 'BEGIN {FS = ":.*##"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 
 preflight-checks: log
+ifndef ANSIBLE_INSTALLED
+	$(error "Ansible is not installed, please install")
+endif
+ifndef TOFU_INSTALLED
+	$(error "OpenTofu is not installed, please install")
+endif
 	@if [[ ! -f "./ansible/group_vars/$(ENVIRONMENT)/main.yml" ]]; then \
 	 	echo "Group vars directory for environment missing" && exit 1; \
 	fi
