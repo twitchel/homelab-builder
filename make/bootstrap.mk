abe06e0956bb120e7b74310a42d85882f8c519e9
Daniel Jones <daniel.jones@oohmedia.com.au>
2024-12-31 18:03:23 +1100

    feat: add more required tools for make install target

--- make/bootstrap.mk
+++ make/bootstrap.mk
@@ -2,16 +2,20 @@
 ##@ Bootstrapping
 #####################
 
-INSTALL_TOOLS=ansible jq yq opentofu
+INSTALL_TOOLS_APT=ansible jq yq opentofu apache2-utils
+INSTALL_TOOLS_BREW=ansible jq yq opentofu
 
 install: log ## Install required tooling on your local machine to run/manage this application stack
 	@echo "Installing dependencies"
 	@if command -v brew > /dev/null 2>&1; then \
-        brew install $(INSTALL_TOOLS); \
+        brew install $(INSTALL_TOOLS_BREW); \
  	fi; \
     if command -v apt > /dev/null 2>&1; then \
-		sudo apt-get install -y $(INSTALL_TOOLS); \
+		sudo apt-get install -y $(INSTALL_TOOLS_APT); \
 	fi;
+	@echo "Installing Ansible addons"
+	@ansible-galaxy collection install gluster.gluster
+	@ansible-galaxy collection install community.general
 
 configure: log ## Run through configuration script to bootstrap your environment
 	@echo "Configuring Homelab Builder Environment"
