3dc9fd6a085c5726108f391befe3a9e5e82aca5a
Daniel Jones <daniel.jones@oohmedia.com.au>
2025-01-14 08:51:59 +1100

    feat: split out infra provision steps from the build step

--- make/infrastructure.mk
+++ make/infrastructure.mk
@@ -6,6 +6,10 @@ infra-build: log confirm preflight-checks ## Builds the base level VM infrastruc
 	@echo "🚧 Building infrastructure"
 	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml ansible/infrastructure-build.yml
 
+infra-provision: log confirm preflight-checks ## Provisions the nodes used for the swarm (install software)
+	@echo "🚧 Provisioning nodes"
+	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml ansible/infrastructure-provision.yml
+
 infra-destroy: log confirm-danger preflight-checks ## Destroys the entire VM swarm stack, along with all your data
 	@echo "🧨 Destroying infrastructure"
 	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml ansible/infrastructure-destroy.yml
