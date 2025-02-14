c5abd188bc361f97dae0520fa787aa9653269279
Daniel K. Jones <daniel.jones@oohmedia.com.au>
2025-02-14 16:39:59 +1100

    Chore/refactor playbooks (#1)
    * feat: move infra-build to proper role dir
    
    * feat: rename swarm nodes to cluster nodes
    
    * feat: split provision for docker vs kubernetes
    
    * feat: rename swarm_role to cluster_role
    
    * fix: docker setup import roles for provision
    
    * feat: add environment name into config for tagging nodes and images
    
    * feat: refactor config
    
    - Use inventory namespace
    - Set example inventory
    - Update build and provision steps to interact with new config
    
    * feat: Update service recipes to work with new config
    
    * feat: update config to move domain under top level service config
    
    * feat: add ability to disable service
    
    * fix: Set correct outpost URL on authetik setup
    
    * chore: lint all files for build play
    
    * chore: lint all files for provision play
    
    * chore: missing lint fixes
    
    * feat: lint + refactor infra-destroy role
    
    * fix: Fix dir permissions for infra-build
    
    * fix: run shell commands in bash for pipefail safety
    
    * chore: tidy up role import syntax
    
    * feat: move traefik deploy to deploy infra services role
    
    * feat: Replace traefik deploy with infra service deploy in stack-deploy playbook
    
    * feat: Replace cloudflared deploy with infra service deploy in stack-deploy playbook
    
    * fix: add missing service.env files
    
    * feat: Replace authentik deploy with infra service deploy in stack-deploy playbook
    
    * feat: Add reusable role for adding authentik auth
    
    * feat: allow authentik add-auth to be called multiple times
    
    * feat: set authentik API facts
    
    * feat: Add reusable role for homelab service deployments
    
    * fix: Minor fixes to deploy process
    
    * feat: migrate actual budget to new deploy
    
    * chore: remove erroneous debug statement
    
    * feat: env vars written from group vars to service .env
    
    * feat: remove comment
    
    * feat: migrate gitea to new deploy
    
    * fix: incorrect global env name
    
    * feat: migrate homepage to new deploy
    
    * feat: migrate overseerr to new deploy
    
    * feat: migrate portainer to new deploy
    
    * fix: Fix incorrect service port map in authentik traefik config
    
    * fix: portainer env
    
    * feat: Add external service mounting
    
    * feat: update example config
    
    * chore: tidy up old code
    
    * feat: add make target to redeploy a service
    
    * fix: Fix issues in example group_vars

--- make/stack-management.mk
+++ make/stack-management.mk
@@ -4,7 +4,13 @@
 
 deploy: log preflight-checks confirm ## This will deploy the swarm stack to your hosts. This can be rerun to add new services
 	@echo "🥞 Deploying Homelab Stack"
-	@ansible-playbook -i config/$(ENVIRONMENT)/hosts.yml ansible/stack-deploy.yml
+	@ENVIRONMENT=$(ENVIRONMENT) ansible-playbook -i ansible/inventory/$(ENVIRONMENT)/hosts.yml ansible/stack-deploy.yml
+
+service-redeploy: log confirm
+	@echo "️🏠 Enter the name of the service you wish to redeploy: " && \
+    	read service_redeploy && \
+		echo "🥞 Redeploying service: $${service_redeploy}" && \
+		ENVIRONMENT=$(ENVIRONMENT) ansible-playbook -i ansible/inventory/$(ENVIRONMENT)/hosts.yml --extra-vars="service_name=$${service_redeploy}" ansible/service-redeploy.yml
 
 deploy-sensors: log preflight-checks confirm ## This will deploy the sensor stack (Zigbee/Zwave) to the defined host.
 	@echo "🤖 Deploying sensors"
