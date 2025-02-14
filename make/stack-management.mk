72c844e52947aba1983822c3cd79c860a0ce0354
Daniel Jones <daniel.jones@oohmedia.com.au>
2025-02-14 16:06:54 +1100

    feat: add make target to redeploy a service

--- make/stack-management.mk
+++ make/stack-management.mk
@@ -6,6 +6,12 @@ deploy: log preflight-checks confirm ## This will deploy the swarm stack to your
 	@echo "🥞 Deploying Homelab Stack"
 	@ENVIRONMENT=$(ENVIRONMENT) ansible-playbook -i ansible/inventory/$(ENVIRONMENT)/hosts.yml ansible/stack-deploy.yml
 
+service-redeploy: log confirm
+	@echo "️🏠 Enter the name of the service you wish to redeploy: " && \
+    	read service_redeploy && \
+		echo "🥞 Redeploying service: $${service_redeploy}" && \
+		ENVIRONMENT=$(ENVIRONMENT) ansible-playbook -i ansible/inventory/$(ENVIRONMENT)/hosts.yml --extra-vars="service_name=$${service_redeploy}" ansible/service-redeploy.yml
+
 deploy-sensors: log preflight-checks confirm ## This will deploy the sensor stack (Zigbee/Zwave) to the defined host.
 	@echo "🤖 Deploying sensors"
 	@echo "NOT IMPLEMENTED YET"
